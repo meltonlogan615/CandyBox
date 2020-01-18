5ffd48c9bb22cf27ec390f7500365ef9de43724e
meltonalogan615 <meltonalogan@me.com>
2020-01-18 13:14:21 -0600

    rando's

---
+++ JS/candyBarFactory.js
@@ -0,0 +1,17 @@
+// Candy Bar Factory
+
+const candyBar = (UPC, RegionA, RegionB, RegionC, BrandParent, BrandName, Variation, Description, Weight, Ingredients, Allergens, NutritionFacts, Rating, UserRating, Seasonal, Website, Image, KeyWords) =>  {
+	return {
+		image,
+		BrandParent,
+		BrandName.
+		Variation,
+		Description,
+		Ingredients,
+		Weight,
+		Allergens,
+		Rating,
+		UserRating,
+		Seasonal,
+	}
+}
