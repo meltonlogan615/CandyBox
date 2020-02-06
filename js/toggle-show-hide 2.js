3ec2443d684b92bd37a80263d58c381212295637
meltonalogan615 <meltonalogan@me.com>
2020-02-05 21:40:02 -0600

    fix js folder

---
+++ js/toggle-show-hide 2.js
@@ -0,0 +1,14 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "none") {
+      panel.style.display="inline-flex";
+    } else {
+      panel.style.display="none";
+    }
+  });
+}
